ec0d5674e2d6cf6dbfbd08d991856c85af7e30a4
Laplaza1 <skysavage55@gmail.com>
2025-11-14 11:50:49 -0600

    dashboards works

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -12,6 +12,20 @@ document.addEventListener("DOMContentLoaded",(()=>
                     document.getElementById("itemNums").textContent = nm
                     document.getElementById("homeNums").textContent = data.Number_of_homes
                     document.getElementById("userNums").textContent = data.Number_of_users
+                    data.Pending_users.forEach(atomo =>
+                        {
+                            console.log(atomo)
+                            let pendapp = document.getElementById("pendApp").cloneNode(true)
+                            pendapp.className = "container"
+                            pendapp.querySelector("#userName").textContent = atomo.username
+                            pendapp.querySelector("#email").textContent = atomo.email
+                            pendapp.querySelector("#passWord").textContent = atomo.password
+                            pendapp.querySelector("#phoneNumber").textContent = atomo.phone_number
+                            pendapp.querySelector("#Reason").textContent = atomo.reason
+                            document.getElementById("pendApps").appendChild(pendapp)
+                        })
+
+
                     console.log(data)
                     console.log(data.Item_count)
                     Object.entries(data.Item_count).forEach(item=>{console.log(item);console.log(yValues[xValues.indexOf(item[0])]);yValues[xValues.indexOf(item[0])]=item[1];console.log(yValues[xValues.indexOf(item[0])]);})
@@ -20,7 +34,25 @@ document.addEventListener("DOMContentLoaded",(()=>
                 }
                 myChart.update()
             })
-         
+         document.querySelectorAll("#collapse_button").forEach(async (e)=>
+                {
+                    console.log(e)
+                    e.addEventListener("click",async (event)=>
+                        {
+                            console.log("clicked")   
+                            let x = event.target.parentElement.querySelector("#pendApps").style.display
+                            
+                            if(x=="none")
+                                {
+                                    event.target.parentElement.querySelector("#pendApps").style.display="block"
+
+                                }
+                            else
+                                {
+                                    event.target.parentElement.querySelector("#pendApps").style.display = "none"
+                                }
+                        })
+                })
 
 
 
